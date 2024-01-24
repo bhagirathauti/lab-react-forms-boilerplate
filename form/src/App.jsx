c5dff455aec1047f8e7b558525c16056995b6a8e
bhagirathauti <bhagirath961778@gmail.com>
2024-01-24 16:35:47 +0530

    .

---
+++ form/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Forms from './Components/Forms'
+function App() {
+
+  return (
+    <>
+      <Forms/>
+    </>
+  )
+}
+
+export default App
